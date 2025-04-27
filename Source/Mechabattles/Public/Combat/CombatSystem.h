0e779c20e435211da22458d7ce7e003d2d6665a3
nektpol <nekocreativetechnologies@gmail.com>
2025-04-27 11:04:42 +0300

    Pathfinding bugfixes

--- Source/Mechabattles/Public/Combat/CombatSystem.h
+++ Source/Mechabattles/Public/Combat/CombatSystem.h
@@ -20,17 +20,12 @@ public:
 	ACombatSystem();
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Mecha)
-	TArray<AMechaActor*> MechasInCombat;
 
 	
 
 protected:
 	
 	virtual void BeginPlay() override;
-
-private:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = References)
-	TObjectPtr<AGrid> Grid = nullptr;
+	
 
 };
