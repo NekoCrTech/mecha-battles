941f3b7c8c4dd2662017f721f5fbec71d21858b6
nektpol <nekocreativetechnologies@gmail.com>
2025-05-02 11:15:35 +0300

    Update Battle Camera

--- Source/Mechabattles/Private/Game/BattleCameraPawn.cpp
+++ Source/Mechabattles/Private/Game/BattleCameraPawn.cpp
@@ -4,6 +4,7 @@
 #include "Game/BattleCameraPawn.h"
 
 #include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
@@ -11,18 +12,26 @@ ABattleCameraPawn::ABattleCameraPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Create Spring Arm for Camera
-	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArmComponent->SetupAttachment(RootComponent);
-	SpringArmComponent->TargetArmLength = 2000.0f; // Default distance from target
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+	
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = DefaultZoom;
+	CameraBoom->bDoCollisionTest = false;
+	CameraBoom->SetWorldRotation(FRotator(-60.f,0.f,0.f));
 
-	// Create Camera
-	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	CameraComponent->SetupAttachment(SpringArmComponent);
+	// Create a camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	
 
-	// Set this pawn to be controlled by the player
-	bReplicates = true;
+}
 
+void ABattleCameraPawn::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
 }
 
 // Called when the game starts or when spawned
@@ -37,6 +46,12 @@ void ABattleCameraPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	Super::Tick(DeltaTime);
+
+	CameraBoom->TargetArmLength = FMath::FInterpTo(CameraBoom->TargetArmLength,ZoomDesired,DeltaTime,ZoomInterpSpeed);
+	SetActorLocation(FMath::VInterpTo(GetActorLocation(),LocationDesired,DeltaTime,MoveInterpSpeed));
+	SetActorRotation(FMath::RInterpTo(GetActorRotation(),RotationDesired,DeltaTime,RotateInterpSpeed));
+
 }
 
 // Called to bind functionality to input
@@ -44,5 +59,77 @@ void ABattleCameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(EnableMoreAction,ETriggerEvent::Started,this,&ABattleCameraPawn::EnableMore);
+		EnhancedInputComponent->BindAction(EnableMoreAction,ETriggerEvent::Completed,this,&ABattleCameraPawn::DisableMore);
+		EnhancedInputComponent->BindAction(EnableMoreAction,ETriggerEvent::Canceled,this,&ABattleCameraPawn::DisableMore);
+
+		EnhancedInputComponent->BindAction(ResetZoomAction, ETriggerEvent::Started,this,&ABattleCameraPawn::ResetZoom);
+		EnhancedInputComponent->BindAction(ResetRotateAction, ETriggerEvent::Started, this, &ABattleCameraPawn::ResetRotate);
+		
+		EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered,this, &ABattleCameraPawn::Rotate);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABattleCameraPawn::Move);
+		EnhancedInputComponent->BindAction(ZoomAction, ETriggerEvent::Triggered, this, &ABattleCameraPawn::Zoom);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+
+}
+
+void ABattleCameraPawn::Zoom(const FInputActionValue& Value)
+{
+	float ZoomAxis = - Value.Get<float>();
+
+	ZoomDesired = ZoomDesired + ZoomAxis * ZoomSpeed;
+	ZoomDesired = FMath::Clamp(ZoomDesired, MinZoom, MaxZoom);
+}
+
+void ABattleCameraPawn::Move(const FInputActionValue& Value)
+{
+	FVector2D MoveAxis = Value.Get<FVector2D>();
+
+	LocationDesired = LocationDesired + MoveAxis.X * MoveSpeed * GetActorForwardVector() + MoveAxis.Y * MoveSpeed * GetActorRightVector();
+}
+
+void ABattleCameraPawn::Rotate(const FInputActionValue& Value)
+{
+	float RotateAxis = Value.Get<float>();
+	if (RotateAxis > 0.f)
+	{
+		RotationDesired = GetActorRotation() + RotateStep;
+	}
+	else if (RotateAxis < 0.f)
+	{
+		RotationDesired = GetActorRotation() - RotateStep;
+	}
+}
+
+void ABattleCameraPawn::EnableMore(const FInputActionValue& Value)
+{
+	bMoreIsEnabled = true;
+}
+
+void ABattleCameraPawn::DisableMore(const FInputActionValue& Value)
+{
+	bMoreIsEnabled = false;
+}
+
+void ABattleCameraPawn::ResetZoom(const FInputActionValue& Value)
+{
+	if (!bMoreIsEnabled)
+	{
+		ZoomDesired = DefaultZoom;
+	}
+}
+
+void ABattleCameraPawn::ResetRotate(const FInputActionValue& Value)
+{
+	if (bMoreIsEnabled)
+	{
+		RotationDesired = DefaultRotation;
+	}
 }
 
--- Source/Mechabattles/Public/Game/BattleCameraPawn.h
+++ Source/Mechabattles/Public/Game/BattleCameraPawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputMappingContext.h"
 #include "BattleCameraPawn.generated.h"
 
 class USpringArmComponent;
@@ -16,16 +17,119 @@ class MECHABATTLES_API ABattleCameraPawn : public APawn
 
 public:
 	ABattleCameraPawn();
-	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyControllerChanged() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(VisibleAnywhere)
-	UCameraComponent* CameraComponent;
-
-	UPROPERTY(VisibleAnywhere)
-	USpringArmComponent* SpringArmComponent;
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetMoveSpeed() const { return MoveSpeed; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetMoveSpeed(const float Value) { MoveSpeed = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetMoveInterpSpeed() const { return MoveInterpSpeed; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetMoveInterpSpeed(const float Value) { MoveInterpSpeed = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetRotateStep() const { return RotateStep.Yaw; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetRotateStep(const float Value) { RotateStep.Yaw = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetRotateInterpSpeed() const { return RotateInterpSpeed; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetRotateInterpSpeed(const float Value) { RotateInterpSpeed = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetZoomSpeed() const { return ZoomSpeed; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetZoomSpeed(const float Value) { ZoomSpeed = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetMinZoom() const { return MinZoom; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetMinZoom(const float Value) { MinZoom = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetMaxZoom() const { return MaxZoom; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetMaxZoom(const float Value) { MaxZoom = Value; }
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FORCEINLINE float GetZoomInterpSpeed() const { return ZoomInterpSpeed; }
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetZoomInterpSpeed(const float Value) { ZoomInterpSpeed = Value; }
+	
 
 protected:
+	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void Zoom(const FInputActionValue& Value);
+	void Move(const FInputActionValue& Value);
+	void Rotate(const FInputActionValue& Value);
+	void EnableMore(const FInputActionValue& Value);
+	void DisableMore(const FInputActionValue& Value);
+	void ResetZoom(const FInputActionValue& Value);
+	void ResetRotate(const FInputActionValue& Value);
+
+
+private:
+
+	/** Components */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* Root;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Input Actions */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ZoomAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* RotateAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* EnableMoreAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ResetZoomAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ResetRotateAction;
+	
+	/** Settings **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Move", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 20.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Move", meta = (AllowPrivateAccess = "true"))
+	float MoveInterpSpeed = 5.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Rotate", meta = (AllowPrivateAccess = "true"))
+	FRotator RotateStep = FRotator(0.f,45.f,0.f);
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Rotate", meta = (AllowPrivateAccess = "true"))
+	FRotator DefaultRotation = GetActorRotation();
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Rotate", meta = (AllowPrivateAccess = "true"))
+	float RotateInterpSpeed = 2.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Zoom", meta = (AllowPrivateAccess = "true"))
+	float ZoomSpeed = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Zoom", meta = (AllowPrivateAccess = "true"))
+	float MinZoom = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Zoom", meta = (AllowPrivateAccess = "true"))
+	float MaxZoom = 5000.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Zoom", meta = (AllowPrivateAccess = "true"))
+	float DefaultZoom = 3500.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings | Zoom", meta = (AllowPrivateAccess = "true"))
+	float ZoomInterpSpeed = 2.f;
+	
+	FRotator RotationDesired = GetActorRotation();
+	float ZoomDesired = DefaultZoom;
+	FVector LocationDesired = GetActorLocation();
+	bool bMoreIsEnabled = false;
 };
